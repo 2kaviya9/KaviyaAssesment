58bfd0166bed826d54f5d72e63540bcd25734786
2kaviya9 <kaviya1571@gmail.com>
2024-05-26 22:39:28 +0530

    Revert "first commit"
    This reverts commit 7117f4b42e59782b6f70ace20a10717f4c3fbf9f.

--- destination/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
-from .models import Destination
-from .serializers import DestinationSerializer
-
-class DestinationViewset(viewsets.ModelViewSet):
-    permission_classes = (AllowAny,)
-    queryset = Destination.objects.all()
-    serializer_class = DestinationSerializer
